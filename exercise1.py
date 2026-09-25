bf9355aca912efe3345ca4ae6ad1603c7fdb8ae4
scott <hericscott033@gmail.com>
2026-09-25 11:18:40 +0100

    showing the values of the following expression

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+#Exercise 1
+#To what values do the following expressions evaluate?
+#False or (True and False)
+#True or (1 + 2)
+#(1 + 2) or True
+#True and (1 + 2)
+#False and (1 + 2)
+#(1 + 2) and True
+#(32 * 4) >= 129
+#False != (not True)
+#True == 4
+#False == (847 == '847')
+
+#solution 
+
+print("1:", False or (True and False))
+print("2:", True or (1 + 2))
+print("3:", (1 + 2) or True)
+print("4:", True and (1 + 2))
+print("5:", False and (1 + 2))
+print("6:", (1 + 2) and True)
+print("7:", (32 * 4) >= 129)
+print("8:", False != (not True))
+print("9:", True == 4)
+print("10:", False == (847 == '847'))
